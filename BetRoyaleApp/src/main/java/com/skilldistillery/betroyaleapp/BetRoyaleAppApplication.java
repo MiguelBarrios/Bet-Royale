555cab083a4eac5783efba20157febcdc0fc1a9c
Lpaladini90 <lpaladini@me.com>
2022-04-22 08:59:52 -0700

    Create Spring Boot Project, Add JPA project Dependency

---
+++ BetRoyaleApp/src/main/java/com/skilldistillery/betroyaleapp/BetRoyaleAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.betroyaleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetRoyaleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetRoyaleAppApplication.class, args);
+	}
+
+}
