7b792b4c0aaacdd858a826990c158f4147b3ac15
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-04-22 18:54:11 -0500

    changes

--- JPABetRoyale/src/main/java/com/skilldistillery/betroyaleapp/entities/ViewingParty.java
+++ JPABetRoyale/src/main/java/com/skilldistillery/betroyaleapp/entities/ViewingParty.java
@@ -1,9 +1,26 @@
 package com.skilldistillery.betroyaleapp.entities;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class ViewingParty {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
+	private String title;
+	
+	private String description;
+	
+	private LocalDate startDate;
+	
+	private LocalTime startTime;
 
 }
