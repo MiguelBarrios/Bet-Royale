228bd44424ee3c0a67a3ebda23c6eb2045fab9f0
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-04-22 17:39:10 -0500

    wager entity updated

--- JPABetRoyale/src/main/java/com/skilldistillery/betroyaleapp/entities/Wager.java
+++ JPABetRoyale/src/main/java/com/skilldistillery/betroyaleapp/entities/Wager.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-
 public class Wager {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +14,41 @@ public class Wager {
 	
 	@Column(name="bet_amount")
 	private double betAmount;
+	
+	private double odds;
+	
+	public Wager() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getBetAmount() {
+		return betAmount;
+	}
+
+	public void setBetAmount(double betAmount) {
+		this.betAmount = betAmount;
+	}
+
+	public double getOdds() {
+		return odds;
+	}
+
+	public void setOdds(double odds) {
+		this.odds = odds;
+	}
+
+	@Override
+	public String toString() {
+		return "Wager [id=" + id + ", betAmount=" + betAmount + ", odds=" + odds + "]";
+	}
+	
+	
 	
 
 }
---
+++ JPABetRoyale/src/test/java/com/skilldistillery/betroyaleapp/entities/WagerTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.betroyaleapp.entities;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WagerTest {
+
+	private static EntityManagerFactory emf;
+	
+	private EntityManager em;
+	
+	private Wager wager;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = emf = Persistence.createEntityManagerFactory("VideoStore");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		 emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	    em = emf.createEntityManager();
+	    wager = em.find(Wager.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	    em = emf.createEntityManager();
+	    wager = em.find(Wager.class, 1);
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
