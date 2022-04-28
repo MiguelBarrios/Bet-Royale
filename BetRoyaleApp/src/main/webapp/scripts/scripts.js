18159e57f71911e0669acc70dd2b65ec882f3566
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-04-28 12:05:21 -0500

    finished linking tabes in profile page

---
+++ BetRoyaleApp/src/main/webapp/scripts/scripts.js
@@ -0,0 +1,65 @@
+function loadDisplay(clicked_id) {
+    console.log(clicked_id)
+    var selectedview = clicked_id + "view";
+    var views = ["loadAllBetsview",
+        "createEventview",
+        "leaderboardview",
+        "myWagersview",
+        "updateUserview"
+    ];
+    for (let i = 0; i < views.length; i++) {
+        if (views[i] == selectedview) {
+            document
+                .getElementById(selectedview).classList
+                .add("active");
+
+        } else {
+            document
+                .getElementById(views[i]).classList
+                .remove("active");
+        }
+    }
+
+    var viewCard = document
+        .getElementById("View")
+    console.log(viewCard)
+}
+
+function addContender() {
+    $("#contender-names")
+        .append(
+            '<input type="text" name="contenderName">');
+    $("#contender-odds")
+        .append(
+            ' <input type="number" name="contenderOdds" min="1", max="100" step="1">')
+}
+
+function addsubcategory() {
+    $('#sub-cateory-container')
+        .append(
+            '<input type="text" name="cname">')
+}
+
+function toggleContender(clicked_id) {
+    var contender = document
+        .getElementById(clicked_id).innerHTML;
+    document
+        .getElementById("contenderName").value = contender;
+    var contenderBtn = document
+        .getElementById(clicked_id);
+    contenderBtn.id;
+    var contId = document
+        .getElementById("contenderId");
+    if (contenderBtn.classList
+        .contains("btn-secondary")) {
+        contenderBtn.classList
+            .remove("btn-secondary")
+        contenderBtn.classList
+            .add("btn-primary");
+    } else {
+        contenderBtn.classList
+            .remove("btn-primary");
+        contenderBtn.classList
+            .add("btn-secondary");
+    }
+}
